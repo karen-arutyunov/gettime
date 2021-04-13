534b9c13e65216fac06ed60af2f48446bc59d2d4
Karen Arutyunov <karen@codesynthesis.com>
2021-04-13 13:26:42 +0300

    Initalize tm object

--- gettime/gettime.cxx
+++ gettime/gettime.cxx
@@ -7,8 +7,11 @@ int main (int argc, char* argv[])
 {
   using namespace std;
 
-  tm t;
+  tm t {};
   istringstream is ("2016");
+
+  is.imbue (locale (setlocale (LC_ALL, nullptr)));
+
   is >> get_time (&t, "%Y %d");
 
   cout << "eof:  " << is.eof ()  << endl
